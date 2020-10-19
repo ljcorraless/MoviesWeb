60154fc388a3a300afc51c82e949db62cfe440ca
Luis Corrales Solano <luis.corrales1197@gmail.com>
2020-10-19 00:15:04 -0600

    Added views and controllers

---
+++ MoviesWeb/MoviesWeb/Controllers/LoginController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MoviesWeb.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: LoginController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
